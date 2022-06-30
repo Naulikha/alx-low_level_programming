ccf21e9bbd0680aa247fd996e0981e95d862d887
Naulikha <nbiketia@gmail.com>
2022-06-30 16:52:18 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <unlstd.h>
 int main(void)
 {
-fwrite(2, "and that place oof art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that place oof art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
